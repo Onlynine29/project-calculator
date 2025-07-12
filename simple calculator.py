cf3ee19e7de3f620cfadf9c34981ac72559c5ec4
Onlynine29 <121753941+Onlynine29@users.noreply.github.com>
2025-07-12 13:03:04 +0530

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -1,3 +1,8 @@
+# Simple Python Calculator
+# Author: Varun Goud
+# Description: Performs basic arithmetic (+, -, *, /) between two numbers
+
+# Define the main calculation logic
 def calculate(num1,operator,num2):
     if operator == "+":
         return num1 + num2
@@ -11,9 +16,12 @@ def calculate(num1,operator,num2):
         return num1 / num2
     else:
         return "invalid operator"
+# Take inputs from the user
 num1 = float(input("enter a number :"))
 operator = input("select the operator (+, -, *, /):")
 num2 = float(input("enter a number :")) 
 
+# Perform the calculation
 result = calculate(num1,operator,num2)
+# Display the result
 print(f"{num1} {operator} {num2} = {result}")
